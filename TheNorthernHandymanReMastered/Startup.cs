bd3b4b5da2d19e2d2d7f3ee1cdbd699b00c69c96
Authorty <itismejody@GMAIL.COM>
2018-05-09 17:23:27 -0500

    Add project files.

---
+++ TheNorthernHandymanReMastered/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheNorthernHandymanReMastered.Startup))]
+namespace TheNorthernHandymanReMastered
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
