3e3111c4417759f7bbaebc5228810a0ecfe6e644
Authorty <itismejody@GMAIL.COM>
2018-05-27 14:35:24 -0500

    Got the base of everything workign and added back in

--- TheNorthernHandymanReMastered/Controllers/HomeController.cs
+++ TheNorthernHandymanReMastered/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mail;
 using System.Web.Mvc;
+using TheNorthernHandymanReMastered.Models;
 
 namespace TheNorthernHandymanReMastered.Controllers
 {
@@ -21,10 +23,117 @@ namespace TheNorthernHandymanReMastered.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(ContactEmailModels e)
         {
-            ViewBag.Message = "Your contact page.";
+            string ValidationMessage = String.Empty;
 
+            ViewBag.Name = String.Empty;
+            ViewBag.Email = String.Empty;
+            ViewBag.Phone = String.Empty;
+            ViewBag.Message = String.Empty;
+
+
+            if (!String.IsNullOrEmpty(e.Name) && !String.IsNullOrEmpty(e.Message) && !String.IsNullOrEmpty(e.Phone) && !String.IsNullOrEmpty(e.Email))
+            {
+                var emailmessage = new System.Web.Mail.MailMessage()
+                {
+                    Subject = e.Name,
+                    Body = "From: " + e.Name + "\n Phone Number: " + e.Phone + "\n Job Description: " + e.Message + "\n Email: " + e.Email,
+                    From = "thenorthernhandyman@thenorthernhandyman.org",
+                    To = "itismejody@gmail.com",
+                    BodyFormat = MailFormat.Text,
+                    Priority = System.Web.Mail.MailPriority.High
+                };
+
+                System.Web.Mail.SmtpMail.SmtpServer = "relay-hosting.secureserver.net";
+                SmtpMail.Send(emailmessage);
+                MessageBox("Email sent successfully!");
+                //return RedirectToAction("Index", "Home", null);
+
+            }
+
+            return View(e);
+        }
+        public EmptyResult MessageBox(string s)
+        {
+            Response.Write("<script type=\"text/javascript\">alert('" + s + "');</script>");
+            return null;
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+            return View();
+        }
+        public ActionResult Videos()
+        {
+            return View();
+        }
+        public ActionResult Brush_Remvoal_Hogging()
+        {
+            return View();
+        }
+        public ActionResult Limb_Removal()
+        {
+            return View();
+        }
+        public ActionResult Mowing()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Stump_Grinding()
+        {
+            return View();
+        }
+        public ActionResult Tree_Removal()
+        {
+            return View();
+        }
+        public ActionResult Trimming()
+        {
+            return View();
+        }
+        public ActionResult AllPhotos()
+        {
+            return View();
+        }
+        public ActionResult Tree_Stump_Removal_Index()
+        {
+            return View();
+        }
+        public ActionResult Landscape_Lawncare_Index()
+        {
+            return View();
+        }
+        public ActionResult Winter_Services_Index()
+        {
+            return View();
+        }
+        public ActionResult LeafRemoval()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg6()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg7()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg8()
+        {
+            return View();
+        }
+        public ActionResult Portfolio_Modal1()
+        {
             return View();
         }
         //public ActionResult 
