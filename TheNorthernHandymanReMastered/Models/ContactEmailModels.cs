73a3f93e4e91ee92997d6999cbd8a897705c9de1
Authorty <itismejody@GMAIL.COM>
2018-05-29 20:33:37 -0500

    Swapped about and contact page up

--- TheNorthernHandymanReMastered/Models/ContactEmailModels.cs
+++ TheNorthernHandymanReMastered/Models/ContactEmailModels.cs
@@ -8,12 +8,19 @@ namespace TheNorthernHandymanReMastered.Models
 {
     public class ContactEmailModels
     {
+        [MinLength(4,ErrorMessage ="Minimum of 4 charecters")]
         [Required]
         public string Name { get; set; }
+
         [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+
         [Required]
+        [MinLength(7, ErrorMessage ="Please fill in 7 digits")]
         public string Phone { get; set; }
+
         [Required]
         public string Message { get; set; }
     }
